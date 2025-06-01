d9643ea725369f2b0a3525d475171fc59f6b9b2f
Winfried Koehler <78253700+wirbel-at-vdr-portal@users.noreply.github.com>
2025-06-01 09:17:13 +0200

    Add files via upload

---
+++ html/chapter08/all.sh
@@ -0,0 +1,132 @@
+#./8.14._Bc-7.0.3.sh
+#
+#sleep 10
+#./8.15._Flex-2.6.4.sh
+#
+#sleep 10
+#./8.16._Tcl-8.6.16.sh
+#
+#sleep 10
+#./8.17._Expect-5.45.4.sh
+#
+#sleep 10
+#./8.18._DejaGNU-1.6.3.sh
+#
+#sleep 10
+#./8.19._Pkgconf-2.4.3.sh
+#
+#sleep 10
+#./8.20._Binutils-2.44.sh
+#
+#sleep 10
+#./8.21._GMP-6.3.0.sh
+#
+#sleep 10
+#./8.22._MPFR-4.2.2.sh
+#
+#sleep 10
+#./8.23._MPC-1.3.1.sh
+#
+#sleep 10
+#./8.24._Attr-2.5.2.sh
+#
+#sleep 10
+#./8.25._Acl-2.3.2.sh
+#
+#sleep 10./8.26._Libcap-2.76.sh
+#
+#sleep 10
+#./8.27._Libxcrypt-4.4.38.sh
+#
+#sleep 10
+#./8.28._Shadow-4.17.4.sh
+#
+#sleep 10
+#./8.29._GCC-14.2.0.sh
+#
+#sleep 10
+#./8.30._Ncurses-6.5.sh
+#
+#sleep 10
+#./8.31._Sed-4.9.sh
+#
+#sleep 10
+#./8.32._Psmisc-23.7.sh
+#
+#sleep 10
+#./8.33._Gettext-0.25.sh
+#
+#sleep 10
+#./8.34._Bison-3.8.2.sh
+#
+#sleep 10
+#./8.35._Grep-3.12.sh
+#
+#sleep 10
+#./8.36._Bash-5.2.37.sh
+
+sleep 10
+./8.37._Libtool-2.5.4.sh
+
+sleep 10
+./8.38._GDBM-1.25.sh
+
+sleep 10
+./8.39._Gperf-3.3.sh
+
+sleep 10
+./8.40._Expat-2.7.1.sh
+
+sleep 10
+./8.41._Inetutils-2.6.sh
+
+sleep 10
+./8.42._Less-668.sh
+
+sleep 10
+./8.43._Perl-5.40.2.sh
+
+sleep 10
+./8.44._XML.Parser-2.47.sh
+
+sleep 10
+./8.45._Intltool-0.51.0.sh
+
+sleep 10
+./8.46._Autoconf-2.72.sh
+
+sleep 10
+./8.47._Automake-1.17.sh
+
+sleep 10
+./8.48._OpenSSL-3.5.0.sh
+
+sleep 10
+./8.49._Libelf_from_Elfutils-0.193.sh
+
+sleep 10
+./8.50._Libffi-3.4.8.sh
+
+sleep 10
+./8.51._Python-3.13.3.sh
+
+sleep 10
+./8.52._Flit-Core-3.12.0.sh
+
+sleep 10
+./8.54._Wheel-0.46.1.sh
+
+sleep 10
+./8.55._Setuptools-80.7.1.sh
+
+sleep 10
+./8.56._Ninja-1.12.1.sh
+
+sleep 10
+./8.57._Meson-1.8.0.sh
+
+sleep 10
+./8.58._Kmod-34.2.sh
+
+sleep 10
+./8.59._Coreutils-9.7.sh
